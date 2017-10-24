a81d9b47db8d60d764a1de6ccd05be9fab6c59de
ipunganwar <ipung.anwar1@gmail.com>
2017-10-24 20:02:42 +0700

    release 1

---
+++ cookie.js
@@ -0,0 +1,29 @@
+'use strict'
+const Ingredients = require('./ingredient')
+
+
+class Cookie{
+	constructor(name, arr_ingredients){
+		let ingredients = new Ingredients()
+		this.name = name
+		this.status = "mentah"
+		this.ingredients = ingredients.split_ingredient(arr_ingredients)
+		this.has_sugar = ingredients.cek_sugar()
+	}
+
+	bake(str){
+		this.status = str
+		return this.status
+		
+	}
+}
+
+
+
+module.exports = Cookie
+
+// let cookie = new Cookie('tes', [ ' 1 cup : flour',
+//        ' 2 cups : sugar',
+//        ' 2 cups : cinnamon',
+//        ' 1 tlbsp : butter' ])
+// console.log(cookie)
